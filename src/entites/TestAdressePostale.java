e8e0a951a1517868fd20443b79d6a24024428844
benours <benjamin.baska@gmail.com>
2021-10-06 23:08:03 +0200

    fin tp1

--- src/entites/TestAdressePostale.java
+++ src/entites/TestAdressePostale.java
@@ -7,6 +7,7 @@ public class TestAdressePostale {
 		AdressePostale a1 = new AdressePostale(13, "rue des froux", 33000, "Cityville");
 		AdressePostale a2 = new AdressePostale();
 		System.out.println(a1.toString());
+		System.out.println(a2.toString());
 	}
 
 }
