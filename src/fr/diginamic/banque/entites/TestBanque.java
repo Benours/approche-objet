68b504dd433c2ce298d8d37bc1a0c41169d149fa
benours <benjamin.baska@gmail.com>
2021-10-06 23:36:34 +0200

    tp6 fini

--- src/fr/diginamic/banque/entites/TestBanque.java
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -5,7 +5,7 @@ public class TestBanque {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Compte c1 = new Compte(180697, 13000);
-		System.out.println(c1.toString());
+		System.out.println(c1.toString()); //tp6
 	}
 
 }
