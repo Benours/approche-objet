ee98719c83e7873a82cdca478887f35f7d350cb7
benours <benjamin.baska@gmail.com>
2021-10-06 23:26:27 +0200

    tp4 fini

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,15 @@
+package utils;
+import java.lang.Integer;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String chaine = "12";
+		int valeur = Integer.parseInt(chaine);
+		int a = 5;
+		int b = 12;
+		System.out.println(Integer.max(a, b));
+	}
+
+}
