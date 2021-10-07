e66b5357dc79d495070b1f06aec6b82e48570a06
benours <benjamin.baska@gmail.com>
2021-10-07 11:44:08 +0200

    Operation fini

---
+++ src/fr/diginamic/essais/TestOperations.java
@@ -0,0 +1,22 @@
+package fr.diginamic.essais;
+
+import fr.diginamic.operations.*;
+
+public class TestOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double nb1 = (Math.random() * 100);
+		double nb2 = (Math.random() * 100);
+		double resultat;
+		resultat = Operations.calcul(nb1, nb2, '+');
+		System.out.println("Addition : " + resultat);
+		resultat = Operations.calcul(nb1, nb2, '-');
+		System.out.println("Soustraction : " + resultat);
+		resultat = Operations.calcul(nb1, nb2, '*');
+		System.out.println("Multiplication : " + resultat);
+		resultat = Operations.calcul(nb1, nb2, '/');
+		System.out.println("Division : " + resultat);
+	}
+
+}
---
+++ src/fr/diginamic/operations/Operations.java
@@ -0,0 +1,22 @@
+package fr.diginamic.operations;
+
+public class Operations {
+	public static double calcul(double a, double b, char c) {
+		
+		if (c == '+') {
+			return a + b;
+		}
+		else if (c == '-') {
+			return a - b;
+		}
+		else if (c == '*') {
+			return a * b;
+		}
+		else if (c == '/') {
+			return a / b;
+		}
+		else {
+			return -0.0;
+		}
+	}
+}
